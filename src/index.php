3bbf1a82a8a0cc09b0e3bdafb0bee95d1494f037
SafakB <safakbahce@gmail.com>
2024-05-18 11:20:39 +0300

    update init and test env

--- src/index.php
+++ src/index.php
@@ -1,31 +1,23 @@
 <?php
-
 require_once(__DIR__ . '/../vendor/autoload.php');
 
-// use Appwrite\Client;
-// use Appwrite\Exception;
 
-// This is your Appwrite function
-// It's executed each time we get a request
 return function ($context) {
-    // Why not try the Appwrite SDK?
-    //
-    // $client = new Client();
-    // $client
-    //     ->setEndpoint('https://cloud.appwrite.io/v1')
-    //     ->setProject(getenv('APPWRITE_FUNCTION_PROJECT_ID'))
-    //      ->setKey(getenv('APPWRITE_API_KEY'));
 
-    // You can log messages to the console
-    $context->log('Hello, Logs!');
+    throw_if_missing(
+        $_ENV,
+        [
+            'OZTEK_TEST_PHONE',
+        ]
+    );
 
-    // If something goes wrong, log an error
+    $OZTEK_TEST_PHONE = $_ENV['OZTEK_TEST_PHONE'];
+   
+    $context->log('Hello, Logs!' . $OZTEK_TEST_PHONE);   
     $context->error('Hello, Errors!');
 
-    // The `req` object contains the request data
-    if ($context->req->method === 'GET') {
-        // Send a response with the res object helpers
-        // `res.send()` dispatches a string back to the client
+
+    if ($context->req->method === 'GET') {     
         return $context->res->send('Hello, World!');
     }
 
